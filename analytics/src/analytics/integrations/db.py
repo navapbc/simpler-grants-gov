b609b91cec8335265875b18b5c886a80d0a50173
Alsia Plybeah <alsia@navapbc.com>
2024-05-22 09:53:30 -0400

    import SQL methods

---
+++ analytics/src/analytics/integrations/db.py
@@ -0,0 +1,23 @@
+# pylint: disable=invalid-name
+"""Get a connection to the database using a SQLAlchemy engine object."""
+
+from sqlalchemy import Engine, create_engine
+
+from config import settings
+
+
+def get_db() -> Engine:
+    """
+    Get a connection to the database using a SQLAlchemy engine object.
+
+    This function retrieves the database connection URL from the configuration
+    and creates a SQLAlchemy engine object.
+
+    Yields
+    ------
+    sqlalchemy.engine.Engine
+    A SQLAlchemy engine object representing the connection to the database.
+    """
+    return create_engine(settings.database_url, pool_pre_ping=True)
+# get connection strings in here; this only has client connection
+# what would these connection strings be?

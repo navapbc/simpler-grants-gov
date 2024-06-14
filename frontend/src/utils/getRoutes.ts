2cfbd20cc60818aab01c86362a4248c2d3c2923a
Ryan Lewis <ryanlewis@navapbc.com>
2024-06-13 17:39:52 -0700

    remove console log

--- frontend/src/utils/getRoutes.ts
+++ frontend/src/utils/getRoutes.ts
@@ -35,6 +35,6 @@ export function getNextRoutes(src: string): string[] {
       : "/";
     return route.replace(/\/\//g, "/");
   });
-  console.log("appRoutes => ", appRoutes);
+
   return appRoutes;
 }

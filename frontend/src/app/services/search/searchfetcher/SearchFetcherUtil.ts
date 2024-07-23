3a62b6fb47e2fa22f924cf2af855a7bc36c95176
acouch <acinternets@gmail.com>
2024-07-23 14:04:47 -0400

    Remove duplicate folder

--- frontend/src/app/services/search/searchfetcher/SearchFetcherUtil.ts
+++
@@ -1,9 +0,0 @@
-import { APISearchFetcher } from "./APISearchFetcher";
-import { MockSearchFetcher } from "./MockSearchFetcher";
-import { SearchFetcher } from "./SearchFetcher";
-
-export const getSearchFetcher = (): SearchFetcher => {
-  return process.env.USE_SEARCH_MOCK_DATA === "true"
-    ? new MockSearchFetcher()
-    : new APISearchFetcher();
-};

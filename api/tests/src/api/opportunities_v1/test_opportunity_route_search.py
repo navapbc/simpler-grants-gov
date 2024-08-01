9ceca5f657ee4c193c7c0f91558f9ea02645ec50
Rwolfe-Nava <richardwolfe@navapbc.com>
2024-08-01 16:45:15 -0400

    add additional test cases

--- api/tests/src/api/opportunities_v1/test_opportunity_route_search.py
+++ api/tests/src/api/opportunities_v1/test_opportunity_route_search.py
@@ -723,7 +723,14 @@ class TestOpportunityRouteSearch(BaseTestClass):
             (get_search_request(post_date={"start_date": None, "end_date": "2020-02-01"}), 200),
             (get_search_request(post_date={"start_date": "2020-01-01", "end_date": "2020-02-01"}), 200),
             (get_search_request(post_date={"start_date": "I am not a date"}), 422),
+            (get_search_request(post_date={"start_date": "123-456-789"}), 422),
+            (get_search_request(post_date={"start_date": "5"}), 422),
+            (get_search_request(post_date={"start_date": 5}), 422),
             (get_search_request(post_date={"end_date": "I am not a date"}), 422),
+            (get_search_request(post_date={"end_date": "123-456-789"}), 422),
+            (get_search_request(post_date={"end_date": "5"}), 422),
+            (get_search_request(post_date={"end_date": 5}), 422),
+
             # Close Date
             (get_search_request(close_date={"start_date": None}), 200),
             (get_search_request(close_date={"end_date": None}), 200),

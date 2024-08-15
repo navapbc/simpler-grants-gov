e580f99a8937ae8d02bd3634ec2656111e0f068e
Rwolfe-Nava <87499456+Rwolfe-Nava@users.noreply.github.com>
2024-08-15 14:47:24 -0400

    Update comment
    Co-authored-by: Michael Chouinard <46358556+chouinar@users.noreply.github.com>

--- api/tests/src/task/opportunities/test_export_opportunity_data_task.py
+++ api/tests/src/task/opportunities/test_export_opportunity_data_task.py
@@ -38,7 +38,7 @@ class TestExportOpportunityDataTask(BaseTestClass):
             OpportunityFactory.create_batch(size=6, is_archived_forecast_summary=True)
         )
 
-        # Create some opportunities that won't get fetched / loaded into search
+        # Create some opportunities that won't get fetched / exported
         OpportunityFactory.create_batch(size=3, is_draft=True)
         OpportunityFactory.create_batch(size=4, no_current_summary=True)
 

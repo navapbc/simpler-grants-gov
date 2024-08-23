ef254a985a4147202304bcf04a183e21272fd983
Brandon Tabaska <btabaska@gmail.com>
2024-08-23 12:12:20 -0400

    fixing formatting

--- frontend/src/components/search/SearchPagination.tsx
+++ frontend/src/components/search/SearchPagination.tsx
@@ -45,16 +45,17 @@ export default function SearchPagination({
   return (
     <div className={`grants-pagination ${loading ? "disabled" : ""}`}>
       {pages > 0 && (
-      <Pagination
-        pathname="/search"
-        totalPages={pages}
-        currentPage={page}
-        maxSlots={MAX_SLOTS}
-        onClickNext={() => updatePage(page + 1)}
-        onClickPrevious={() => updatePage(page > 1 ? page - 1 : 0)}
-        onClickPageNumber={(event: React.MouseEvent, page: number) =>
-          updatePage(page)}
-      />
+        <Pagination
+          pathname="/search"
+          totalPages={pages}
+          currentPage={page}
+          maxSlots={MAX_SLOTS}
+          onClickNext={() => updatePage(page + 1)}
+          onClickPrevious={() => updatePage(page > 1 ? page - 1 : 0)}
+          onClickPageNumber={(event: React.MouseEvent, page: number) =>
+            updatePage(page)
+          }
+        />
       )}
     </div>
   );
